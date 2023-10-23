a2a581fc7f0cab61ebd2f1d9e150a534fae0e893
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2023-10-23 17:31:16 -0600

    add paquetes

---
+++ rgm-mail/src/main/java/gt/gob/rgm/mail/service/MailService.java
@@ -0,0 +1,12 @@
+package gt.gob.rgm.mail.service;
+
+import gt.gob.rgm.mail.domain.MailTO;
+
+public interface MailService {
+	public void sendMail();
+	
+	public int sendMail(int idTipoMensaje, int idAccountSmtp, String to, String cc, String cco, String subject, String message);
+	
+	public void sendSingleMail(int id);
+
+}
